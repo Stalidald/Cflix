2827b954a6fcbe68bd251b508e4dc57f527dfa5d
Szvetnik Ferenc <d737ge@inf.elte.hu>
2020-03-01 21:57:03 +0100

    init project

---
+++ CataflixBackEnd/src/test/java/hu/elte/CataflixBackEnd/CataflixBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.CataflixBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CataflixBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
