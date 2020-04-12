a4c69b73d53e778f86e759c096d43eb717a4f5b1
tomi19990305 <tomi19990305@gmail.com>
2020-04-12 17:21:41 +0200

    teszt

--- CataflixBackEnd/src/main/java/hu/elte/CataflixBackEnd/security/WebSecurityConfig.java
+++ CataflixBackEnd/src/main/java/hu/elte/CataflixBackEnd/security/WebSecurityConfig.java
@@ -57,7 +57,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll().anyRequest().permitAll();
+                .antMatchers("/api/test/**").permitAll()
+                .anyRequest().permitAll();
         http.headers().frameOptions().disable();
                 //.anyRequest().authenticated();
 
--- CataflixBackEnd/src/main/resources/data.sql
+++ CataflixBackEnd/src/main/resources/data.sql
@@ -1,2 +1,3 @@
 
 INSERT INTO achivement (name, description, version) VALUES ('A', 'B', 0);
+INSERT INTO achivement (name, description, version) VALUES ('B', 'C', 0);
